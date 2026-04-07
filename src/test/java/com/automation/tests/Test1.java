f0e459f17a12f7234a23545ab49691e85768a87d
Xincheng Zhang <xincheng.zhang.dev@gmail.com>
2026-04-08 00:06:41 +0200

    Enhance Test1 and Test2 with additional logging for search actions and assertions

--- src/test/java/com/automation/tests/Test1.java
+++ src/test/java/com/automation/tests/Test1.java
@@ -10,10 +10,15 @@ public class Test1 extends BaseTest {
     @Test
     @Description("Search 'lemonde' on DuckDuckGo and verify navigation to lemonde.fr")
     public void searchForLemonde() {
+        log("Assert search input is visible");
         assertVisible(SearchPage.SEARCH_INPUT);
+        log("Type 'lemonde' and submit search");
         typeAndSubmit(SearchPage.SEARCH_INPUT, "lemonde");
+        log("Assert first result is visible");
         assertVisible(SearchPage.FIRST_RESULT);
+        log("Click first result");
         click(SearchPage.FIRST_RESULT);
+        log("Assert URL contains: " + TestConstants.LEMONDE_URL);
         assertUrlContains(TestConstants.LEMONDE_URL);
     }
 }
--- src/test/java/com/automation/tests/Test2.java
+++ src/test/java/com/automation/tests/Test2.java
@@ -10,10 +10,15 @@ public class Test2 extends BaseTest {
     @Test
     @Description("Search 'lefigaro' on DuckDuckGo and verify navigation to lefigaro.fr")
     public void searchForLefigaro() {
+        log("Assert search input is visible");
         assertVisible(SearchPage.SEARCH_INPUT);
+        log("Type 'lefigaro' and submit search");
         typeAndSubmit(SearchPage.SEARCH_INPUT, "lefigaro");
+        log("Assert first result is visible");
         assertVisible(SearchPage.FIRST_RESULT);
+        log("Click first result");
         click(SearchPage.FIRST_RESULT);
+        log("Assert URL contains: " + TestConstants.LEFIGARO_URL);
         assertUrlContains(TestConstants.LEFIGARO_URL);
     }
 }
